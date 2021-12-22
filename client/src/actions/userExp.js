03618e7c6cd56016eb3919fa88fd9a00f16e6177
Yossi Rivkin <yyr736@gmail.com>
2021-12-22 12:41:44 +0200

    server dir

--- client/src/actions/userExp.js
+++
@@ -1,6 +0,0 @@
-import { SWITCH_MODE } from '../constants/actionTypes';
-
-export const switchDarkMode = (mode) => (dispatch) => {
-  let newMode = mode === 'light' ? 'dark' : 'light';
-  dispatch({ type: SWITCH_MODE, payload: newMode});
-};
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import auth from './auth'
-import stands from './stands';
-import userExp from './userExp';
-
-export const reducers = combineReducers({ stands, auth, userExp });
